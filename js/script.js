f8235542156e56b83feed66e14be52f0fc9387a2
Diego Melo <diego-melo@uni9.edu.br>
2023-10-02 21:55:03 -0300

    1201

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("Teste")
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link){
+    // pega o valor dentro da url
+    const url = location.href;
+    // pega o valor dentro do href
+    const href = link.href;
+
+    // verificar se dentro da url tem o valor href
+    if(url.includes(href)){
+        // adiciona classe ativo
+        link.classList.add("ativo");
+    }
+}
+
+links.forEach(ativarLink)
